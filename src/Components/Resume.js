0e24a40cfd0e39edc33b0641c220736402fb49f6
Marc Sànchez <msanxes@gmail.com>
2020-10-20 20:49:28 +0200

    [FEAT] Sort function for skills and scores, new degree scores feature

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,6 +5,7 @@ class Resume extends Component {
 
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
+      var scoresmessage = this.props.data.scoresmessage;
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
@@ -16,9 +17,43 @@ class Resume extends Component {
             <p>{work.description}</p>
         </div>
       })
-      var skills = this.props.data.skills.map(function(skills){
+      
+      let compare = (scoreA, scoreB, annotation) => {
+        console.log(scoreA)
+        let numberA = scoreA.level.substring(0, scoreA.level.length - 1)
+        let numberB = scoreB.level.substring(0, scoreB.level.length - 1)
+        return (parseInt(numberB) + ( annotation ? scoreB.annotation ? 1 : 0 : 0)) - (parseInt(numberA) + (annotation ? scoreA.annotation ? 1 : 0 : 0))
+      }
+
+
+      var skills = this.props.data.skills.sort(compare).map(function(skills){
         var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
+        return <li key={skills.name}><span style={{width:skills.level}} className={className}></span><em>{skills.name}</em></li>
+      })
+
+      /* let compare = (scoreA, scoreB) => {
+        console.log(scoreA)
+        let numberA = scoreA.level.substring(0, scoreA.level.length - 1)
+        let numberB = scoreB.level.substring(0, scoreB.level.length - 1)
+        return (parseInt(numberB) + (scoreB.annotation ? 1 : 0)) - (parseInt(numberA) + (scoreA.annotation ? 1 : 0))
+      } */
+
+      var scores = this.props.data.scores.sort(compare).map(function(score) {
+      var className = 'bar-expand '+score.name.toLowerCase();
+      return <li key={score.name} style={{position: "relative"}}>
+                <span style={{width:score.level}} className={className}></span>
+                <em>
+                  {score.name + " - " + score.level}
+                </em>
+                {score.annotation && <i className="fa fa-star fa-flip-vertical" style={{
+                    color:"yellow",
+                    position: "absolute",
+                    left: "-0.3em",
+                    top: "-0.3em",
+                    fontSize: "2.5em"
+                    
+                  }} aria-hidden="true"></i>}  
+              </li>
       })
     }
 
@@ -53,24 +88,53 @@ class Resume extends Component {
 
 
 
-      <div className="row skill">
+    <div className="row skill">
 
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
+      <div className="three columns header-col">
+        <h1><span>Skills</span></h1>
       </div>
+
+      <div className="nine columns main-col">
+
+        <p>{skillmessage}</p>
+        <div className="bars">
+            <ul className="skills">
+            {skills}
+          </ul>
+        </div>
+
+        
+      </div>
+    </div>
+
+    <div className="row skill">
+
+        <div className="three columns header-col">
+          <h1><span>Degree Scores</span></h1>
+        </div>
+
+        <div className="nine columns main-col">
+
+          <p>{scoresmessage}</p>
+          <p>
+            <i className="fa fa-star fa-flip-vertical" style={
+                {
+                        color:"yellow",
+                        fontSize: "2.5em"
+                }
+              } aria-hidden="true"></i> 
+            <em> stands for With Honors</em>
+
+          </p>
+          <div className="bars">
+              <ul className="skills">
+                {scores}
+              </ul>
+          </div>
+
+      
+        </div>
+    </div>
    </section>
     );
   }
