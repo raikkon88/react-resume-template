49bdd61e246901ec9731977956ddeb9f0dd04073
Marc Sànchez <msanxes@gmail.com>
2024-02-04 16:17:36 +0100

    feat: adding amplify backend

---
+++ amplify/backend/function/curriculumData/src/index.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    const response = await axios.get('https://msanxes-general-bucket.s3.amazonaws.com/resumeData.json');
+    console.log(`EVENT: ${JSON.stringify(response.data)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+        body: response.data,
+    };
+};
+
+
+https://olcuweu413.execute-api.us-east-1.amazonaws.com/staging/data
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "curriculumData": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "curriculumData": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
