fb51cca08e2c9feafe2d0026543c776951aa61a8
Marc Sànchez <msanxes@gmail.com>
2024-09-23 18:02:54 +0200

    feat: added case where link is not working

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -11,18 +11,39 @@ class Portfolio extends Component {
                         className="columns portfolio-item"
                     >
                         <div className="item-wrap">
-                            <a href={projects.url} title={projects.title}>
-                                <img alt={projects.title} src={projectImage} />
-                                <div className="overlay">
-                                    <div className="portfolio-item-meta">
-                                        <h5>{projects.title}</h5>
-                                        <p>{projects.category}</p>
+                            {projects.url ? (
+                                <a href={projects.url} title={projects.title}>
+                                    <img
+                                        alt={projects.title}
+                                        src={projectImage}
+                                    />
+                                    <div className="overlay">
+                                        <div className="portfolio-item-meta">
+                                            <h5>{projects.title}</h5>
+                                            <p>{projects.category}</p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="link-icon">
-                                    <i className="fa fa-link"></i>
-                                </div>
-                            </a>
+                                    <div className="link-icon">
+                                        <i className="fa fa-link"></i>
+                                    </div>
+                                </a>
+                            ) : (
+                                <>
+                                    <img
+                                        alt={projects.title}
+                                        src={projectImage}
+                                    />
+                                    <div className="overlay">
+                                        <div className="portfolio-item-meta">
+                                            <h5>{projects.title}</h5>
+                                            <p>{projects.category}</p>
+                                        </div>
+                                    </div>
+                                    <div className="link-icon">
+                                        <i className="fa fa-link"></i>
+                                    </div>
+                                </>
+                            )}
                         </div>
                     </div>
                 )
